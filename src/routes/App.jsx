fe39f20ad3bb12d8208ee41ab0326fbfe49b1160
juan david M <juan_da-mari@hotmail.com>
2022-08-25 21:49:42 -0500

    cambio de nombres

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Layout from '../containers/layout';
-import Login from '../containers/login';
+import Login from '../pages/login';
 import '../styles/global.css';
-import RecoveryPassword from '../components/recoveryPassword';
+import RecoveryPassword from '../pages/recoveryPassword';
 import Home from '../pages/home';
 import NotFound from '../pages/NotFound';
-import CreateAcount from '../containers/CreateAcount';
+import CreateAcount from '../pages/CreateAcount';
 import Product from '../pages/Products';
 import Desktop from '../pages/Desktop-Menu';
 import Mobile_Menu from '../components/Mobile-Menu';
@@ -22,6 +22,7 @@ const App = () => {
 				<Routes>
 				   <Route exact path="/" element={ <Home /> } ></Route>
 				   <Route exact path="/login" element={ <Login /> } /> {/* es la forma actual de llamar el componente */}
+				   <Route exact path="/recoveryPassword" element={ <RecoveryPassword /> } />
 				   <Route exact path="/createacount" element={ <CreateAcount />} />
 				   <Route exact path="/Products" element={ <Product/>} />
 				   <Route exact path="/Mobile-menu" element={ <Mobile_Menu/>} />
@@ -29,7 +30,6 @@ const App = () => {
 				   <Route exact path="/Product-detail" element={ <Product_detail/>} />
 				   <Route exact path="/Desktop-Menu" element={ <Desktop/>} />
 				   <Route exact path="/My-orderM" element={ <My_orderM/>} />
-				   <Route exact path="/recoveryPassword" element={ <RecoveryPassword /> } />
 				   <Route path="*" element={ <NotFound /> } ></Route>				
 				</Routes>
 				</switch>
